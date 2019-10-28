d0783cf58d4b0209e8464d162f549f6c7eb98108
andyzen619 <andyzen619@gmail.com>
2019-10-28 19:43:07 +0000

    Added new url form as well as utility.j to hold all utility functions

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+const utility = require("./utility");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -20,15 +23,20 @@ app.get("/urls", (req, res) => {
   });
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   res.render("urls_show", {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL]
   })
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
---
+++ utility.js
@@ -0,0 +1,13 @@
+const alphanumeric = "abcdefghijklmnopqrstuvwxyz1234567890";
+
+const generateRandomString = function() {
+  let result = "";
+  for (let i = 0; i < 6; i++) {
+    result = result.concat(alphanumeric[Math.floor(Math.random() * alphanumeric.length)]);
+  }
+  return result;
+}
+
+module.exports = { generateRandomString }
+
+console.log(generateRandomString());
