8e109798d777869d40ae523a17521adffb2fb9a6
andyzen619 <andyzen619@gmail.com>
2019-10-28 17:49:35 +0000

    Added express server code

--- express_server.js
+++ express_server.js
@@ -7,8 +7,23 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const routObject = {
+  "/": "Hello",
+  "/urls.json": urlDatabase,
+  "/hello": "<html><body>Hello <b>World</b></body></html>\n",
+
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
