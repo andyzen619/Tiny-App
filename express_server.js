3c8644493e5567ab0fff11184aff18661804f8fc
andyzen619 <andyzen619@gmail.com>
2019-10-29 02:18:12 +0000

    Added edit and delete url object entry functionality

--- express_server.js
+++ express_server.js
@@ -27,19 +27,6 @@ app.get('/urls/new', (req, res) => {
   res.render("urls_new");
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  let shortUrl = utility.generateRandomString();
-
-  if (!(Object.keys(urlDatabase).includes(shortUrl))) {
-    let longURL = req.body.longURL;
-
-    urlDatabase[shortUrl] = "https://" + longURL;
-  }
-  console.log(urlDatabase);
-  res.redirect("/urls"); // Respond with 'Ok' (we will replace this)
-});
-
 app.get("/u/:shortURL", (req, res) => {
   console.log(req.params.shortURL);
   const shortURL = req.params.shortURL;
@@ -54,11 +41,32 @@ app.get('/urls/:shortURL', (req, res) => {
   })
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  let shortUrl = utility.generateRandomString();
+
+  if (!(Object.keys(urlDatabase).includes(shortUrl))) {
+    let longURL = req.body.longURL;
+
+    urlDatabase[shortUrl] = "https://" + longURL;
+  }
+  console.log(urlDatabase);
+  res.redirect("/urls"); // Respond with 'Ok' (we will replace this)
+});
+
 app.post('/urls/:shortURL/delete', (req, res) => {
   const shortUrl = req.params.shortURL;
   delete urlDatabase[shortUrl];
   console.log(urlDatabase);
   res.redirect('/urls');
+})
+
+app.post("/urls/:shortURL", (req, res) => {
+  const shortUrl = req.params.shortURL;
+  if (urlDatabase[shortUrl] !== null) {
+    urlDatabase[shortUrl] = req.body.updatedLongURL;
+  }
+  res.redirect("/urls");
 })
 
 app.listen(PORT, () => {
