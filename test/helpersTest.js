5afe93f7dfaf3e9645a122d075fdd26edc8d9e6b
andyzen619 <andyzen619@gmail.com>
2019-10-31 03:35:15 +0000

    Added Mocha and Chai test libraries, wrote two test cases that test the utility.findUserByEmail functionality

---
+++ test/helpersTest.js
@@ -0,0 +1,35 @@
+const { assert } = require('chai');
+
+const utility = require('../utility.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+
+  it('should return a user with valid email', function() {
+    const user = utility.getUserByEmail(testUsers, "user@example.com");
+    const input = user.id;
+    const expectedOutput = "userRandomID";
+    // Write your assert statement here
+
+    assert.equal(input, expectedOutput);
+  });
+
+  it('returns undefined when passed with and non existing email', () => {
+    const input = utility.getUserByEmail(testUsers, "fake@example.com");
+    const expectedOutput = undefined;
+
+    assert.equal(input, expectedOutput);
+  })
+});
